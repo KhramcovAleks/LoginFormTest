cd1854399a14a795371f36c89e42ece435dad666
Александр Храмков <aleksandr.khramkov@simbirsoft.com>
2025-04-09 20:00:59 +0400

    обновил селекторы и добавил проверк

--- src/test/java/TextPracticeForm.java
+++ src/test/java/TextPracticeForm.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -19,33 +20,52 @@ public class TextPracticeForm {
     }
 
     @Test
-    void fillFormTest() {
-        open("/automation-practice-form");
+    void PracticeFormTest() {
+        String userName = "Alex";
 
-        $("#firstName").setValue("Alex");
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+        //удаляем мешающие элементы
+        executeJavaScript("$('footer').remove()");
+
+        $("#firstName").setValue(userName);
         $("#lastName").setValue("Egorov");
         $("#userEmail").sendKeys("alex@egorov.com");
-        $("#gender-radio-3").sendKeys(" ");
+        //$("#gender-radio-3").sendKeys(" ");
         //$("label[for='gender-radio-1']").click();
+        //$("#gender-radio-3").parent().click();
+        $("#genterWrapper").$(byText("Other")).click();
         $("#userNumber").sendKeys("1234567890");
+
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-dropdown-container").$(byText("March")).click();
-        $(".react-datepicker__year-dropdown-container").$(byText("1980")).click();
-        $(".react-datepicker__month-container").$(byText("13")).click();
+        $(".react-datepicker__month-select").selectOption("March");
+        $(".react-datepicker__year-select").selectOption("1980");
+        $(".react-datepicker__day--012:not(.react-datepicker__day--outside-month)").click();
+
         $("#subjectsInput").setValue("English").pressEnter();
-        $("#hobbies-checkbox-1").scrollIntoView(true).sendKeys(" ");
-        $("label[for='hobbies-checkbox-2']").scrollIntoView(true).click();  //Hobbies - Reading
-        $("#uploadPicture").uploadFile(new File("src/test/data/sample.png"));
+        //$("#hobbies-checkbox-1").scrollIntoView(true).sendKeys(" ");
+        //$("label[for='hobbies-checkbox-2']").scrollIntoView(true).click();  //Hobbies - Reading
+        $("#hobbiesWrapper").$(byText("Reading")).scrollIntoView(true).click();
+
+        //$("#uploadPicture").uploadFile(new File("src/test/resources/img/sample.png"));
+        //метод предполагает что файл уже лежит в папке ресурсы
+        $("#uploadPicture").uploadFromClasspath("img/sample.png");
         $("#currentAddress").sendKeys("Some address 1");
+
 
         $("#state").click();
         $("#stateCity-wrapper").$(byText("Uttar Pradesh")).click();
+        //$("#react-select-3-option-0").click();
+
         $("#city").click();
         $("#stateCity-wrapper").$(byText("Agra")).click();
 
          $("#submit").scrollIntoView(true).click();
         //$("#submit").sendKeys(Keys.ENTER);
 
+        //проверка что появилось модальное окно
+        $(".modal-dialog").should(appear);
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
 
         $(".modal-body").shouldHave(text("Alex Egorov"));
